4681a898b96040f1b62f929fb9c368d21b5a5553
Dwarika202249 <dwarikabppimt202249.com>
2023-07-19 10:37:45 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }, [posts])
 
   const fetchBotResponse = async () => {
-    const {data} = await axios.post("http://localhost:4000", {input}, {
+    const {data} = await axios.post("https://chatgpt-clone-s6n2.onrender.com", {input}, {
       headers: {
         "Content-Type": "application/json"
       }
